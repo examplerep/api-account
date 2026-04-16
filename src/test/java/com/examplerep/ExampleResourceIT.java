5349c2ae549ce4db2a0928911d01342d55b42d2e
snimmo <stephennimmo@gmail.com>
2026-04-16 09:57:25 -0500

    project init

---
+++ src/test/java/com/examplerep/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package com.examplerep;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
